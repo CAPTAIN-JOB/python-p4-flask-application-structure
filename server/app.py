9dde1fca618fc9dd4bb0930e8c9bc5c5136364c8
KIPTOO-JOB <job3kiptoo@gmail.com>
2024-10-03 14:57:51 +0300

    done doing  the  router in flask

--- server/app.py
+++ server/app.py
@@ -3,3 +3,22 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return '<h1> Welcome To my page</h1>'
+
+
+# Creating  a  dynamic Route
+
+@app.route('/<string:username>')
+def user(username):
+    return f'<h1> Profile for {username}</h1>'
+
+
+#export FLASK_RUN_PORT=5555
+
+
+if __name__ == '__main__':
+    app.run(port=5555,debug=True)
